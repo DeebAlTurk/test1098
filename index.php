45ff9bd369a4987b7b3489392192ff70a75db01a
DeebAlTurk <66434040+DeebAlTurk@users.noreply.github.com>
2021-08-10 14:50:43 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,18 @@
-<?php echo "hello world";
+<html>
+<head>
+<title>Mariam
+</title>
+  <style>
+    h1{
+    color:red
+    }
+  </style>
+</head>
+<body>
+<h1> This is  an HTML Page </h1>
+</body>
+</html>
+<?php 
+while(true){
+echo "<h1>I LOVE YOU MARIAM</h1><br>";
+}
